3069c744b69dbf7ac1b34d8c75dc5e6a146a00d6
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-03-31 16:31:18 +0200

    Delicious URLs backup

--- delicious-backup.sh
+++ delicious-backup.sh
@@ -1,1 +1,3 @@
+# This doesn't seem to work anymore but https://delicious.com/export allows
+# for exporting bookmarks in HTML with tags and notes
 curl -u $1:$2 https://api.del.icio.us/v1/posts/all > delicious-bookmarks-all.xml
