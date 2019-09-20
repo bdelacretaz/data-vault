5a639fa08ae84c7b748cf09bc016f498445e7396
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-09-20 10:56:40 +0200

    Add limit warning

--- twitter-backup.sh
+++ twitter-backup.sh
@@ -2,9 +2,9 @@
 # Twitter backup script using t gem
 # http://sferik.github.com/t/#configuration explains how to setup your account
 
-# Does Twitter API still set a limit 3200 here?
 LIMIT=${1:-99999}
 USER=${2:-@bdelacretaz}
 
 echo "Backing up up to $LIMIT tweets of $USER to stdout..." >&2
+echo "(but it looks like Twitter sets a lower limit around 3200)" >&2
 t timeline $USER -n $LIMIT -l
