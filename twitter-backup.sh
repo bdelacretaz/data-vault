7463478baa32b134530ebcb321d0eeef71777d7b
Bertrand Delacretaz <bdelacretaz@apache.org>
2020-09-16 16:30:58 +0200

    Today's backups

--- twitter-backup.sh
+++ twitter-backup.sh
@@ -7,4 +7,5 @@ USER=${2:-@bdelacretaz}
 
 echo "Backing up up to $LIMIT tweets of $USER to stdout..." >&2
 echo "(but it looks like Twitter sets a lower limit around 3200)" >&2
+echo "Saving a few overlapping backup files solves that, as a hack" >&2
 t timeline $USER -n $LIMIT -l
